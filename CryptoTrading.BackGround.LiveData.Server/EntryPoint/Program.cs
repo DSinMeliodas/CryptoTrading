bc18c9f3206fad75c15613b9bbbed2072d05e496
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-11-12 13:34:46 +0100

    moved Main method

---
+++ CryptoTrading.BackGround.LiveData.Server/EntryPoint/Program.cs
@@ -0,0 +1,6 @@
+using CryptoTrading.BackGround.LiveData.Server.Core;
+using CryptoTrading.Framework.Util.Services;
+using Microsoft.Extensions.Hosting;
+
+var service  = ServiceBuilder.BuildConfiguredService<LiveDataServer>(args);
+service.Run();
