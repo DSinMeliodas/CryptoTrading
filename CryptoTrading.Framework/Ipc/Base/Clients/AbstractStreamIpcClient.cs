6c11d8729cbb5634f4c682cdf6508eaf165b45f8
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-11-15 14:52:20 +0100

    implemented abstract stream ipc client

---
+++ CryptoTrading.Framework/Ipc/Base/Clients/AbstractStreamIpcClient.cs
@@ -0,0 +1,74 @@
+﻿using CryptoTrading.Framework.Ipc.Interface.Participants;
+using CryptoTrading.Framework.Ipc.Interface.Serialization;
+
+using System;
+using System.IO;
+using System.Threading;
+
+namespace CryptoTrading.Framework.Ipc.Base.Clients
+{
+    internal abstract class AbstractStreamIpcClient<TListenerTarget> : AbstractBaseIpcClient<TListenerTarget>
+        where TListenerTarget : IIpcListenerTarget
+    {
+        private CancellationTokenSource m_CancellationSource;
+
+        protected CancellationToken CancellationToken => m_CancellationSource?.Token ?? new CancellationToken(true);
+
+        protected IIpcCommandDeserializer Deserializer { get; }
+
+        protected IIpcCommandSerializer Serializer { get; }
+
+        protected Stream Stream { get; }
+
+        protected AbstractStreamIpcClient(
+            TimeSpan updateTickRate,
+            Stream underlyingStream,
+            IIpcCommandDeserializer deserializer,
+            IIpcCommandSerializer serializer)
+            : base(updateTickRate)
+        {
+            Deserializer = deserializer;
+            Serializer = serializer;
+            Stream = underlyingStream;
+        }
+
+        public override bool StartListening()
+        {
+            if (!CancellationToken.IsCancellationRequested)
+            {
+                return false;
+            }
+            m_CancellationSource = new CancellationTokenSource();
+            return false;
+        }
+
+        public override bool StopListening()
+        {
+            if (!CancellationToken.IsCancellationRequested)
+            {
+                return false;
+            }
+            m_CancellationSource?.Cancel();
+            return true;
+        }
+
+        protected sealed override void UpdateTick(object _)
+        {
+            var buffer = new byte[Deserializer.BufferSize];
+            _ = Stream.Read(buffer);
+            var command = Deserializer.Deserialize(buffer);
+            ReceiveCommand(command);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (!disposing)
+            {
+                return;
+            }
+            Stream?.Dispose();
+            m_CancellationSource?.Dispose();
+        }
+    }
+}
