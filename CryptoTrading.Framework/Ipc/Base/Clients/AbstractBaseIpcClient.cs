82644299e8f61209c1fbd95f272708f3d8710147
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-11-15 14:51:40 +0100

    implemented abstract base ipc client

---
+++ CryptoTrading.Framework/Ipc/Base/Clients/AbstractBaseIpcClient.cs
@@ -0,0 +1,69 @@
+﻿using CryptoTrading.Framework.Ipc.Interface.Data;
+using CryptoTrading.Framework.Ipc.Interface.Participants;
+
+using System;
+using System.Threading;
+
+namespace CryptoTrading.Framework.Ipc.Base.Clients
+{
+    internal abstract class AbstractBaseIpcClient<TListenerTarget> : IIpcClient<TListenerTarget>
+        where TListenerTarget : IIpcListenerTarget
+    {
+        public event IpcCommandReceived OnCommandReceived;
+
+        private bool m_Disposed;
+        private TimeSpan m_UpdateTickRate;
+
+        private readonly Timer m_Timer;
+
+        public TListenerTarget Target { get; init; }
+
+        protected TimeSpan UpdateTickRate
+        {
+            get=>m_UpdateTickRate;
+            set
+            {
+                m_UpdateTickRate = value;
+                _ = m_Timer.Change(TimeSpan.Zero, UpdateTickRate);
+            }
+        }
+
+        protected AbstractBaseIpcClient(TimeSpan updateTickRate)
+        {
+            m_Timer = new Timer(UpdateTick);
+            UpdateTickRate = updateTickRate;
+        }
+
+        public void Dispose()
+        {
+            Stop();
+            m_Timer?.Dispose();
+            Dispose(!m_Disposed);
+            m_Disposed = true;
+        }
+
+        public virtual void Start()
+        {
+            _ = StartListening();
+        }
+
+        public virtual void Stop()
+        {
+            _ = StopListening();
+        }
+
+        public abstract IIpcCommandResult Send(IIpcCommand command);
+
+        protected abstract void UpdateTick(object _);
+
+        public abstract bool StartListening();
+
+        public abstract bool StopListening();
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        protected void ReceiveCommand(IIpcCommand command) => OnCommandReceived?.Invoke(command);
+    }
+}
