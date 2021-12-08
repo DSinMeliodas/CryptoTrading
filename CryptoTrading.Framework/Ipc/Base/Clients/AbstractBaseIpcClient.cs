5a7b19b8fa730dc5df7441525996d890b5addfe3
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-12-08 11:41:04 +0100

    changed AbstractBaseIpcClient to pass tests

--- CryptoTrading.Framework/Ipc/Base/Clients/AbstractBaseIpcClient.cs
+++ CryptoTrading.Framework/Ipc/Base/Clients/AbstractBaseIpcClient.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoTrading.Framework.Ipc.Interface.DataTransfer;
 using CryptoTrading.Framework.Ipc.Interface.Participants;
+using CryptoTrading.Framework.Parallelism;
 
 using System;
 using System.Threading;
@@ -9,14 +10,26 @@ namespace CryptoTrading.Framework.Ipc.Base.Clients
     internal abstract class AbstractBaseIpcClient<TListenerTarget> : IIpcClient<TListenerTarget>
         where TListenerTarget : IIpcListenerTarget
     {
+        private static readonly TimeSpan Infinite = TimeSpan.FromMilliseconds(-1);
+
         public event IpcCommandReceived<TListenerTarget> OnCommandReceived;
 
         private bool m_Disposed;
         private TimeSpan m_UpdateTickRate;
 
+        private readonly CancellationController m_ListeningController = new();
+        private readonly CancellationController m_RunningController = new();
         private readonly Timer m_Timer;
 
+        public bool IsListening => !ListeningToken.IsCancellationRequested;
+
+        public bool IsRunning => !RunningToken.IsCancellationRequested;
+
         public TListenerTarget Target { get; init; }
+
+        protected CancellationToken ListeningToken => m_ListeningController.Token;
+
+        protected CancellationToken RunningToken => m_RunningController.Token;
 
         protected TimeSpan UpdateTickRate
         {
@@ -31,39 +44,82 @@ namespace CryptoTrading.Framework.Ipc.Base.Clients
         protected AbstractBaseIpcClient(TimeSpan updateTickRate)
         {
             m_Timer = new Timer(UpdateTick);
-            UpdateTickRate = updateTickRate;
+            m_ListeningController.LinkTo(m_RunningController);
+            m_UpdateTickRate = updateTickRate;
         }
 
         public void Dispose()
         {
-            Stop();
+            _ = Stop();
             m_Timer?.Dispose();
+            m_RunningController.Dispose();
+            m_ListeningController.Dispose();
             Dispose(!m_Disposed);
             m_Disposed = true;
         }
 
-        public virtual void Start()
+        public bool Start()
         {
-            _ = StartListening();
+            if (IsRunning || !OnStart() || !StartListeningInternal(true))
+            {
+                return false;
+            }
+            m_RunningController.ChangeSource(new CancellationTokenSource());
+            return true;
         }
 
-        public virtual void Stop()
+        public bool Stop()
         {
-            _ = StopListening();
+            if (!IsRunning || !OnStop() || !StopListening())
+            {
+                return false;
+            }
+            m_RunningController.Reset();
+            return true;
+        }
+
+        public bool StartListening() => StartListeningInternal(false);
+
+        public bool StopListening()
+        {
+            if (!IsListening || !OnListeningStop())
+            {
+                return false;
+            }
+            m_ListeningController.Reset();
+            return m_Timer.Change(Infinite, UpdateTickRate);
         }
 
         public abstract IIpcCommandResult Send(IIpcCommand command);
 
-        protected abstract void UpdateTick(object _);
-
-        public abstract bool StartListening();
-
-        public abstract bool StopListening();
+        protected void ReceiveCommand(IIpcCommand command) => OnCommandReceived?.Invoke(this, command);
 
         protected virtual void Dispose(bool disposing)
         {
         }
 
-        protected void ReceiveCommand(IIpcCommand command) => OnCommandReceived?.Invoke(this, command);
+        protected abstract bool OnListeningStart();
+
+        protected abstract bool OnListeningStop();
+
+        protected abstract bool OnStart();
+
+        protected abstract bool OnStop();
+
+        protected abstract void UpdateTick(object _);
+
+        private bool StartListeningInternal(bool initialStart)
+        {
+            if (!initialStart && !IsRunning)
+            {
+                return false;
+            }
+            if (IsListening || !OnListeningStart())
+            {
+                return false;
+            }
+            m_ListeningController.ChangeSource(new CancellationTokenSource());
+            return m_Timer.Change(TimeSpan.Zero, UpdateTickRate);
+        }
     }
 }
