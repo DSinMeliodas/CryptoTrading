62782ccde3856369ea8e066acb3557e4092e793b
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-12-08 11:45:28 +0100

    implemented event args for AbstractBaseIpcClient

---
+++ CryptoTrading.Framework/EventArgs/ObjectDisposedArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CryptoTrading.Framework.EventArgs
+{
+    internal sealed class ObjectDisposedArgs<TDisposed> : System.EventArgs
+        where TDisposed : IDisposable
+    {
+        public TDisposed DisposedObject { get; }
+
+        public ObjectDisposedArgs(TDisposed disposedObject)
+        {
+            DisposedObject = disposedObject;
+        }
+    }
+}
---
+++ CryptoTrading.Framework/EventArgs/SourceChangedArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptoTrading.Framework.EventArgs
+{
+    internal sealed class SourceChangedArgs<TSource> : System.EventArgs
+    {
+        public TSource OldSource { get; }
+
+        public TSource NewSource { get; }
+
+        public SourceChangedArgs(
+            TSource oldSource,
+            TSource newSource)
+        {
+            OldSource = oldSource;
+            NewSource = newSource;
+        }
+    }
+}
