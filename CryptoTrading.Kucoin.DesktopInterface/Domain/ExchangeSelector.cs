c23bec0599ea27bc4fc963ee01669e3a6c58f9e9
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-16 14:51:10 +0100

    created ExchangeSelector

---
+++ CryptoTrading.Kucoin.DesktopInterface/Domain/ExchangeSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Domain;
+
+internal class ExchangeSelector
+{
+    private const string InplaceInstanceId = nameof(InplaceInstance);
+
+    private static readonly Dictionary<string, ExchangeSelector> m_Selectors = new();
+
+    public ObservableCollection<string> Exchanges { get; private set; }
+
+    private ExchangeSelector(ITickUpdater updater)
+    {
+    }
+
+    internal static ExchangeSelector InplaceInstance()
+    {
+        if(m_Selectors.TryGetValue(InplaceInstanceId, out var selector)) {
+            return selector;
+        }
+        selector = new ExchangeSelector(null);
+        m_Selectors.Add(InplaceInstanceId, selector);
+        return selector;
+    }
+}
