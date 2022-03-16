83c061cd4cebad9abbecac991d1049fdbd97bfff
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-16 14:50:10 +0100

    created ExchangeSelectionViewModel

---
+++ CryptoTrading.Kucoin.DesktopInterface/ViewModel/ExchangeSelectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.ViewModel;
+
+internal sealed class ExchangeSelectionViewModel : BaseViewModel
+{
+    private readonly ExchangeSelector Selector = ExchangeSelector.InplaceInstance();
+
+    public ObservableCollection<string> Exchanges => Selector.Exchanges;
+
+    protected override void Dispose(bool disposing)
+    {
+    }
+}
