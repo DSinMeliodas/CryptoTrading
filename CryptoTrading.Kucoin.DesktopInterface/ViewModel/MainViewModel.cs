369aa12c1f6a9d68278ddce79d0de745b93b7b2c
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-16 14:50:22 +0100

    created MainViewModel

---
+++ CryptoTrading.Kucoin.DesktopInterface/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.Collections;
+using LiveChartsCore.SkiaSharpView;
+
+using System.ComponentModel;
+
+using System.Runtime.CompilerServices;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.ViewModel;
+
+internal class MainViewModel : BaseViewModel
+{
+    public RangeObservableCollection<ISeries> Series { get; } = new ();
+
+    public RangeObservableCollection<Axis> XAxes { get; } = new ();
+
+
+    
+    protected override void Dispose(bool disposing)
+    {
+    }
+
+    private RangeObservableCollection<> selectableExchanges;
+
+    public System.Collections.IEnumerable SelectableExchanges { get => selectableExchanges; set => SetProperty(ref selectableExchanges, value); }
+}
