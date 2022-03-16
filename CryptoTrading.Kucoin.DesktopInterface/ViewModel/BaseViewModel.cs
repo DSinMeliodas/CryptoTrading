ec42533465bd85d7bc75fdf26ed3b1d4072948c4
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-16 14:49:47 +0100

    created BaseViewModel

---
+++ CryptoTrading.Kucoin.DesktopInterface/ViewModel/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CryptoTrading.Kucoin.DesktopInterface.Annotations;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.ViewModel;
+
+internal abstract class BaseViewModel : INotifyPropertyChanged, IDisposable
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected abstract void Dispose(bool disposing);
+}
