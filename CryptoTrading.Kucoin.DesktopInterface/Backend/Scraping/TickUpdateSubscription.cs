d6ed504f52180a22894a28c8ca3ca6ad30398afa
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-17 10:18:55 +0100

    created Subscription

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/TickUpdateSubscription.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping;
+
+public class TickUpdateSubscription
+{
+    private readonly Type m_Type;
+
+    public long Id { get; init; }
+
+    public string Target { get; init; }
+
+    public TickUpdateSubscription(long id, string target, Type type)
+    {
+        Id = id;
+        Target = target;
+        m_Type = type ?? throw new ArgumentNullException(nameof(type));
+    }
+
+    internal bool TryCastSubscribed<T>(object subscribedValue, out T castedValue)
+    {
+        if(typeof(T).IsAssignableFrom(m_Type))
+        {
+            castedValue = (T)subscribedValue;
+            return true;
+        }
+
+        castedValue = default;
+        return false;
+    }
+}
