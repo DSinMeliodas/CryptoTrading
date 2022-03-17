a3e6decbe3a90e2e1696244af14d6be4c32fefd7
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-17 10:18:46 +0100

    created SubscriptionArgs

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/TickUpdateEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping;
+
+public delegate void OnTickUpdate(ITickUpdater sender, TickUpdateEventArgs args);
+
+public class TickUpdateEventArgs : EventArgs
+{
+
+    private readonly IReadOnlyDictionary<TickUpdateSubscription, object> m_Subscriptions;
+
+    public TickUpdateEventArgs(IReadOnlyDictionary<TickUpdateSubscription, object> subscription)
+    {
+        m_Subscriptions = subscription;
+    }
+
+    public bool TryGetSubscriptionResult<T>(TickUpdateSubscription subscription, out T subscriptionValue)
+    {
+        var success = m_Subscriptions.TryGetValue(subscription,  out var result);
+        return subscription.TryCastSubscribed(result, out subscriptionValue);
+    }
+}
