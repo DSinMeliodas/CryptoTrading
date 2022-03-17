a647f8fefbf5da2a91b3af24b39edfaa2900f5af
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-17 12:21:24 +0100

    added Querying classes

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Querying/SubscriptionQuery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CryptoExchange.Net.Objects;
+using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping;
+using Kucoin.Net.Clients;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Querying;
+
+internal class SubscriptionQuery
+{
+    private readonly IEnumerable<TickUpdateSubscription> m_Subscriptions;
+
+    private SubscriptionQuery(IEnumerable<TickUpdateSubscription> subscriptions)
+    {
+        m_Subscriptions = subscriptions;
+    }
+
+    public TaskQuery<WebCallResult> UpdateOn(KucoinClient client)
+    {
+        var groupedByTarget = m_Subscriptions.GroupBy(s => s.Target);
+        var updateOn = groupedByTarget.Select(group => group.Key.UpdateOn(client));
+        return TaskQuery<WebCallResult>.ForAll(updateOn);
+    }
+
+
+    public static SubscriptionQuery All(IEnumerable<TickUpdateSubscription> subscriptions)
+    {
+        return new SubscriptionQuery(subscriptions);
+    }
+}
---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Querying/TaskQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Querying
+{
+    public class TaskQuery<T>
+    {
+        private readonly IEnumerable<Task<T>> m_Tasks;
+
+        private TaskQuery(IEnumerable<Task<T>> tasks)
+        {
+            m_Tasks = tasks;
+        }
+
+        public void OnCompletedAsync(Action<Task<T>> onCompletion)
+        {
+            var completingTasks = m_Tasks.Select(t => t.ContinueWith(onCompletion)).ToArray();
+            Task.WaitAll(completingTasks);
+        }
+
+        public static TaskQuery<T> ForAll(IEnumerable<Task<T>> tasks)
+        {
+            return new TaskQuery<T>(tasks);
+        }
+    }
+}
