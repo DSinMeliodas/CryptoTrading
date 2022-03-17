e20a6cfcd64a64b416670ebec34ba1597133b7b0
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-03-17 13:57:30 +0100

    added TickerTarget interface

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/ITickerTarget.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CryptoExchange.Net.Objects;
+using Kucoin.Net.Clients;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping;
+
+public interface ITickerTarget
+{
+    Task<CallResult<object>> UpdateOn(KucoinClient client);
+}
