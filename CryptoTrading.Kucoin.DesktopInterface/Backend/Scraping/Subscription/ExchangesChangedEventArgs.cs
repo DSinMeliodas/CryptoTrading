c8d5672d424199e15cb982ce1930e558846dc7f4
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-11 21:01:02 +0200

    finished implementation

--- CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/Subscription/ExchangesChangedEventArgs.cs
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/Subscription/ExchangesChangedEventArgs.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Subscription;
 
 public class ExchangesChangedEventArgs : EventArgs
 {
-    
+    public IReadOnlyList<string> Exchanges { get; }
+    public IReadOnlyList<string>? RemovedExchanges { get; }
+
+    public ExchangesChangedEventArgs(IEnumerable<string> exchanges, IEnumerable<string>? removedExchanges = null)
+    {
+        Exchanges = exchanges.ToList();
+        RemovedExchanges = removedExchanges?.ToList();
+    }
 }
