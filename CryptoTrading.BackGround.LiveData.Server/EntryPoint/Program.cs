fafea52d8f65147659e814df971e5dd75140c370
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-14 15:35:06 +0200

    removed irratating projects

--- CryptoTrading.BackGround.LiveData.Server/EntryPoint/Program.cs
+++
@@ -1,6 +0,0 @@
-using CryptoTrading.BackGround.LiveData.Server.Core;
-using CryptoTrading.Framework.Util.Services;
-using Microsoft.Extensions.Hosting;
-
-var service  = ServiceBuilder.BuildConfiguredService<LiveDataServer>(args);
-service.Run();
