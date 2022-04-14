d6e5f3d07a1dafd4d79b5c622d2e00c1ea9e4d43
DSinMeliodas <dragonmelodiassin@gmail.com>
2022-04-14 15:09:22 +0200

    created TickUpdaterTarget for Exchanges

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/Targets/ExchangeTickUpdaterTarget.cs
@@ -0,0 +1,35 @@
+﻿using CryptoExchange.Net.Objects;
+
+using CryptoTrading.Kucoin.DesktopInterface.Domain;
+
+using Kucoin.Net.Clients;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CryptoExchange.Net.CommonObjects;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Targets;
+
+public sealed class ExchangeTickUpdaterTarget : ITickUpdaterTarget
+{
+    private const string UpdaterIdentifierFormat = @"{0}-Updater";
+
+    private readonly ExchangeIdentifier m_Identifier;
+
+    public Type ResultType => typeof(IEnumerable<Kline>);
+
+    public string UpdaterId { get; }
+
+    public ExchangeTickUpdaterTarget(ExchangeIdentifier identifier)
+    {
+        m_Identifier = identifier;
+        UpdaterId = string.Format(UpdaterIdentifierFormat, m_Identifier.Identifier);
+    }
+
+    public async Task<CallResult<object>> UpdateOn(KucoinClient client)
+    {
+        var result = await client.SpotApi.CommonSpotClient.GetKlinesAsync(m_Identifier.Identifier, TimeSpan.MaxValue);
+        return result.As((object)result.Data);
+    }
+}
