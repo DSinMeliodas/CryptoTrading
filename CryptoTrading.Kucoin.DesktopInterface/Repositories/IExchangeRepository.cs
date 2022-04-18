bd086fbbc07f6e2929f437e6c5c64cf5e27734e8
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-18 16:04:44 +0200

    made ExchangeRepository disposable

--- CryptoTrading.Kucoin.DesktopInterface/Repositories/IExchangeRepository.cs
+++ CryptoTrading.Kucoin.DesktopInterface/Repositories/IExchangeRepository.cs
@@ -1,9 +1,11 @@
 ﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
 using CryptoTrading.Kucoin.DesktopInterface.Repositories.CallBacks;
 
+using System;
+
 namespace CryptoTrading.Kucoin.DesktopInterface.Repositories;
 
-public interface IExchangeRepository
+public interface IExchangeRepository : IDisposable
 {
     Exchange GetExchange(string exchangeId, IExchangeUpdateCallBack callBack);
 }
