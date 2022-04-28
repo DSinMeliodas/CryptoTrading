eec92ed45e32e7a613d343492cd9563cb00058dc
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 10:40:49 +0200

    refactored CallBacks

--- CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/Subscription/ExchangesUpdate.cs
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Scraping/Subscription/ExchangesUpdate.cs
@@ -1,5 +1,4 @@
-﻿using CryptoExchange.Net.Objects;
-
+﻿
 using CryptoTrading.Kucoin.DesktopInterface.Backend.Extensions;
 using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Updater;
 
@@ -8,37 +7,22 @@ using Kucoin.Net.Objects.Models.Spot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Subscription;
 
 internal delegate void ExchangesChanged(IReadOnlyCollection<string> currentExchanges);
 
-internal sealed class ExchangesWatcherCallBack : ISubscriptionCallBack
+internal sealed class ExchangesUpdate : SubscriptionCallBackBase
 {
     public event EventHandler<ExchangesChangedEventArgs> OnExchangesChanged;
 
     private readonly List<string> m_Exchanges = new();
 
-    public TickUpdateSubscription Subscription { get; set; }
 
-
-    public void OnTickUpdate(TickUpdateEventArgs args)
+    public override void OnTickUpdate(TickUpdateEventArgs args)
     {
-        ArgumentNullException.ThrowIfNull(args);
-        if (args.IsError)
+        if (!CheckArgs(args, out IEnumerable<KucoinSymbol> subscription))
         {
-#if DEBUG
-            MessageBox.Show(args.Error?.ToString());
-#endif
-            return;
-        }
-        if (!args.TryGetCastedResult(out IEnumerable<KucoinSymbol> subscription))
-        {
-#if DEBUG
-            MessageBox.Show("Failed retrieving result");
-#endif
             return;
         }
 
@@ -54,9 +38,5 @@ internal sealed class ExchangesWatcherCallBack : ISubscriptionCallBack
         m_Exchanges.Clear();
         m_Exchanges.AddRange(exchangeNames);
         OnExchangesChanged?.Invoke(this, new (m_Exchanges, notPresentAnyMore));
-    }
-
-    private void OnExchangeCollection(ITickUpdater sender, TickUpdateEventArgs args)
-    {
     }
 }
