306d53ebcaac1d7d3acd2a79b0dac5247599e661
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 11:46:00 +0200

    implemented callback and request for ExchangeLoading

---
+++ CryptoTrading.Kucoin.DesktopInterface/Repositories/CallBacks/ExchangeLoadedCallback.cs
@@ -0,0 +1,15 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+
+using System;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Repositories.CallBacks;
+
+internal sealed class ExchangeLoadedCallback : IExchangeUpdateCallBack
+{
+    public event Action<Exchange> OnLoaded; 
+
+    public void OnExchangeUpdate(Exchange currentExchange)
+    {
+        OnLoaded?.Invoke(currentExchange);
+    }
+}
---
+++ CryptoTrading.Kucoin.DesktopInterface/UseCases/Requests/ExchangeRequest.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Repositories.CallBacks;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.UseCases.Requests;
+
+internal sealed class ExchangeRequest : IExchangeRequest
+{
+
+    public string ExchangeId { get; }
+
+    public IExchangeUpdateCallBack UpdateCallBack { get; }
+
+    public ExchangeRequest(string exchangeId, IExchangeUpdateCallBack updateCallBack)
+    {
+        ExchangeId = exchangeId;
+        UpdateCallBack = updateCallBack;
+    }
+}
