0f82cd2a9449c4478f225db1ad6da63706b20a51
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 20:18:36 +0200

    implemented RemoveExchange use case

---
+++ CryptoTrading.Kucoin.DesktopInterface/UseCases/RemoveExchanges.cs
@@ -0,0 +1,31 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Backend.Management;
+using CryptoTrading.Kucoin.DesktopInterface.Domain.Factories;
+
+using System.Collections.Generic;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.UseCases;
+
+internal class RemoveExchanges : IContextBasedUseCase<IReadOnlyList<string>>
+{
+    private readonly IExchangeManager m_ExchangeManager;
+
+    public RemoveExchanges(IExchangeManager exchangeManager)
+    {
+        m_ExchangeManager = exchangeManager;
+    }
+
+    public void Execute(IReadOnlyList<string> context)
+    {
+        foreach (var removedExchange in context)
+        {
+            var exchangeFactory = new ExchangeIdentifierFactory(removedExchange);
+            var exchangeId = exchangeFactory.Create();
+            if (exchangeId.Equals(m_ExchangeManager.CurrentExchangeIdentifier))
+            {
+                m_ExchangeManager.CloseCurrentExchange();
+                continue;
+            }
+            m_ExchangeManager.CloseExchange(exchangeId);
+        }
+    }
+}
