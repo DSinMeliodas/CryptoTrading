840ee1745d6c8151090b4f0c3adbc72182d0e4a6
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 20:18:05 +0200

    usding exchange now as contextbased usecase

--- CryptoTrading.Kucoin.DesktopInterface/UseCases/SelectExchange.cs
+++ CryptoTrading.Kucoin.DesktopInterface/UseCases/SelectExchange.cs
@@ -1,38 +1,34 @@
-﻿using CryptoTrading.Kucoin.DesktopInterface.Backend.Exchange;
-using CryptoTrading.Kucoin.DesktopInterface.Domain.Factories;
+﻿using CryptoTrading.Kucoin.DesktopInterface.Backend.Management;
 using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
 using CryptoTrading.Kucoin.DesktopInterface.Specifications;
 
 namespace CryptoTrading.Kucoin.DesktopInterface.UseCases;
 
-internal sealed class SelectExchange 
+internal sealed class SelectExchange : IContextBasedUseCase<Exchange>
 {
     private readonly IExchangeManager m_Manager;
-    private readonly ExchangeIdentifier m_SelectedExchangeIdentifier;
     private readonly ISpecification<ExchangeIdentifier> m_ExchangeIsCurrent;
     private readonly ISpecification<ExchangeIdentifier> m_ExchangeIsOpen;
 
-    public SelectExchange(IExchangeManager manager, string selectedExchangeIdentifier)
+    public SelectExchange(IExchangeManager manager)
     {
         m_Manager = manager;
-        var identifierFactory = new ExchangeIdentifierFactory(selectedExchangeIdentifier);
-        m_SelectedExchangeIdentifier = identifierFactory.Create();
         m_ExchangeIsCurrent = new ExchangeIsCurrent(m_Manager);
         m_ExchangeIsOpen = new ExchangeIsOpen(m_Manager);
     }
 
-    public void Execute()
+    public void Execute(Exchange exchange)
     {
-        if (m_ExchangeIsCurrent.IsMet(m_SelectedExchangeIdentifier))
+        if (m_ExchangeIsCurrent.IsMet(exchange.Identifier))
         {
             return;
         }
-        if (m_ExchangeIsOpen.IsMet(m_SelectedExchangeIdentifier))
+        if (m_ExchangeIsOpen.IsMet(exchange.Identifier))
         {
-            m_Manager.SetOpenedAsCurrent(m_SelectedExchangeIdentifier);
+            m_Manager.SetOpenedAsCurrent(exchange.Identifier);
             return;
         }
 
-        m_Manager.OpenExchange(m_SelectedExchangeIdentifier);
+        m_Manager.OpenExchange(exchange);
     }
 }
