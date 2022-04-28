123e6815e04db0280459f3be71fac7a5ba68a8f0
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 10:05:58 +0200

    implemented KucoinExchangeRepository

---
+++ CryptoTrading.Kucoin.DesktopInterface/Repositories/KucoinExchangeRepository.cs
@@ -0,0 +1,49 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Updater;
+using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+using CryptoTrading.Kucoin.DesktopInterface.Repositories.CallBacks;
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Subscription;
+using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Targets;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Repositories;
+
+internal sealed class KucoinExchangeRepository : IExchangeRepository
+{
+    public static IExchangeRepository SingletonInstance { get; } = new KucoinExchangeRepository();
+
+    private readonly Dictionary<ExchangeIdentifier, Exchange> m_Exchanges = new ();
+    private readonly Dictionary<ExchangeIdentifier, List<IExchangeUpdateCallBack>> m_RegisteredCallBacks = new();
+    private readonly Dictionary<ExchangeIdentifier, TickUpdateSubscription> m_UpdateSubscriptions = new ();
+    private readonly ITickUpdater m_KucoinTickUpdater = new KucoinTickUpdater();
+
+    private KucoinExchangeRepository()
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public async Task<Exchange> GetExchange(ExchangeIdentifier exchangeId, IExchangeUpdateCallBack callBack)
+    {
+        if (m_Exchanges.TryGetValue(exchangeId, out var exchange))
+        {
+            m_RegisteredCallBacks[exchangeId].Add(callBack);
+            return exchange;
+        }
+        return await RegisterAndLoadInitial(exchangeId, callBack);
+    }
+
+    private async Task<Exchange> RegisterAndLoadInitial(ExchangeIdentifier exchangeId, IExchangeUpdateCallBack callBack)
+    {
+        var callBacks = new List<IExchangeUpdateCallBack> { callBack };
+        var subscription = m_KucoinTickUpdater.Subscribe(new ExchangeTickUpdaterTarget(exchangeId),
+                                                            new ExchangeUpdateCallBack(callBacks));
+        m_RegisteredCallBacks[exchangeId] = callBacks;
+        m_UpdateSubscriptions[exchangeId] = subscription;
+        return await m_KucoinTickUpdater.BaseUpdater.GetExchange(exchangeId);
+    }
+}
