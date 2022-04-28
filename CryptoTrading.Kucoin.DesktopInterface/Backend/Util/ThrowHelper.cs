96ae3755a3f23f74aaa71aa498350db417ea39c8
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-28 20:16:58 +0200

    ThrowHelper to remove code duplication

---
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Util/ThrowHelper.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Util;
+
+internal static class ThrowHelper
+{
+    [DoesNotReturn]
+    [StackTraceHidden]
+    public static void ThrowExchangeNotOpen(ExchangeIdentifier exchangeId)
+    {
+        throw new ArgumentException($"exchange {exchangeId} is not open");
+    }
+}
