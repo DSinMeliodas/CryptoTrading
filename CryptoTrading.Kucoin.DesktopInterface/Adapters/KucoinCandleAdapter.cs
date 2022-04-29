361f0423e0e1cecd359577469b24e4d359691cd3
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-29 11:47:33 +0200

    adding Adapters for conversion between ApiDataView and Domain

---
+++ CryptoTrading.Kucoin.DesktopInterface/Adapters/KucoinCandleAdapter.cs
@@ -0,0 +1,20 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+
+using Kucoin.Net.Objects.Models.Spot;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Adapters;
+
+internal sealed class KucoinCandleAdapter : IAdapter<KucoinKline, Candle>
+{
+    public Candle ConvertFrom(KucoinKline value)
+    {
+        return new Candle(
+            value.OpenTime,
+            value.OpenPrice,
+            value.ClosePrice,
+            value.HighPrice,
+            value.LowPrice,
+            value.Volume,
+            value.QuoteVolume);
+    }
+}
---
+++ CryptoTrading.Kucoin.DesktopInterface/Adapters/KucoinExchangeAdapter.cs
@@ -0,0 +1,28 @@
+﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+
+using Kucoin.Net.Enums;
+using Kucoin.Net.Objects.Models.Spot;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Adapters;
+
+internal sealed class KucoinExchangeAdapter : IAdapter<IEnumerable<KucoinKline>, Exchange>
+{
+    private readonly ExchangeIdentifier m_Identifier;
+    private readonly KlineInterval m_Interval;
+
+    public KucoinExchangeAdapter(ExchangeIdentifier identifier, KlineInterval interval)
+    {
+        m_Identifier = identifier;
+        m_Interval = interval;
+    }
+
+    public Exchange ConvertFrom(IEnumerable<KucoinKline> klines)
+    {
+        var candleAdapter = new KucoinCandleAdapter();
+        var candles = klines.Select(candleAdapter.ConvertFrom).ToArray();
+        return new Exchange(m_Identifier, m_Interval, candles);
+    }
+}
