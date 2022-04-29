7f401d394775c54d6fe123709fb8ca27e31fffb8
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-04-29 11:48:23 +0200

    added exeption

--- CryptoTrading.Kucoin.DesktopInterface/Backend/Util/ThrowHelper.cs
+++ CryptoTrading.Kucoin.DesktopInterface/Backend/Util/ThrowHelper.cs
@@ -1,8 +1,12 @@
-﻿using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+﻿using CryptoExchange.Net.Objects;
+
+using CryptoTrading.Kucoin.DesktopInterface.Backend.Scraping.Targets;
+using CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
 
 using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace CryptoTrading.Kucoin.DesktopInterface.Backend.Util;
 
@@ -13,5 +17,22 @@ internal static class ThrowHelper
     public static void ThrowExchangeNotOpen(ExchangeIdentifier exchangeId)
     {
         throw new ArgumentException($"exchange {exchangeId} is not open");
+    }
+    
+    [StackTraceHidden]
+    public static void ThrowIfCallError(CallResult call)
+    {
+        if (!call)
+        {
+            throw new Exception(call.Error!.Message);
+        }
+    }
+
+    public static void ThrowIfUndefined(DataTargetIdentifier identifier, [CallerArgumentExpression("identifier")] string? expression = null)
+    {
+        if (identifier == DataTargetIdentifier.Undefined)
+        {
+            throw new ArgumentException($"{expression} is undefined", expression);
+        }
     }
 }
