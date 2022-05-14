e4c472f037beabf2859fbb15fdcc1f52f0d61bb7
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2022-05-14 18:29:33 +0200

    moved exchange symbol factory and make ctor private

---
+++ CryptoTrading.Kucoin.DesktopInterface/Domain/Records/ExchangeSymbol.Factory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CryptoTrading.Kucoin.DesktopInterface.Domain.Records;
+
+public partial record ExchangeSymbol
+{
+    public class ExchangeSymbolFactory
+    {
+        private static readonly Regex ExchangeSymbolRegex = new(@"[a-zA-Z0-9]{3,5}\-[a-zA-Z0-9]{3,5}");
+        private readonly string m_RawExchangeSymbol;
+
+        public ExchangeSymbolFactory(string rawExchangeSymbol)
+        {
+            m_RawExchangeSymbol = rawExchangeSymbol;
+            if (!ExchangeSymbolRegex.IsMatch(m_RawExchangeSymbol))
+            {
+                throw new ArgumentException(
+                    $"{nameof(rawExchangeSymbol)} does not match regex {nameof(ExchangeSymbolRegex)}");
+            }
+        }
+
+        public ExchangeSymbol Create()
+        {
+            var currencies = m_RawExchangeSymbol.Split('-');
+            return new ExchangeSymbol(m_RawExchangeSymbol, currencies[0], currencies[1]);
+        }
+    }
+}
